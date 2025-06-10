50e9a39de3dc4053d9ba0cdae4426c03b83a2599
ja <jaxa42421@gmail.com>
2025-06-10 16:10:57 +0500

    task3

--- if03.py
+++ if03.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if (a<b and a<c) or (a>b and a<c) :
+        print("a is in the middle")
+    elif (b<a and b<c) or (b<a and b>c):
+        print("b is in the middle")
+    elif (c<a and c<b) or (c<a and c>b):
+        print("c is in the middle")
+    else: 
+        print("all of em are equal")   
