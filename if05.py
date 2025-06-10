f397d626c618af8086c1c81eae5035f1ce8ddca5
ja <jaxa42421@gmail.com>
2025-06-10 16:21:50 +0500

    task5

--- if05.py
+++ if05.py
@@ -6,4 +6,20 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    d1 = n // 10000
+    d2 = (n // 1000) % 10
+    d3 = (n // 100) % 10
+    d4 = (n // 10) % 10
+    d5 = n % 10
+    if d1 >= d2 and d1 >= d3 and d1 >= d4 and d1 >= d5:
+        largest = d1
+    elif d2 >= d3 and d2 >= d4 and d2 >= d5:
+        largest = d2
+    elif d3 >= d4 and d3 >= d5:
+        largest = d3
+    elif d4 >= d5:
+        largest = d4
+    else:
+        largest = d5
+
+        return largest
