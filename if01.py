c52a67f181b34fcaa757ac52401c3228f7e25e92
ja <jaxa42421@gmail.com>
2025-06-10 16:06:53 +0500

    task1

--- if01.py
+++ if01.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a>b and a>c:
+        print("a is the biggest")
+    elif b>a and b>c:
+        print("b is the biggest")
+    elif c>a and c>b:
+        print("c is the biggest")
+    else: 
+        print("all of em are equal")    
